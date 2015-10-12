0a9f8827cec6efc3d96e0c84fd7ab5017acb9cb1
redshiftzero <jen@redshiftzero.com>
2015-10-12 14:43:08 -0500

    Fix metrics in webapp
    display precision at 10% in model summary list

--- evaluation/webapp/config.py
+++ evaluation/webapp/config.py
@@ -1,4 +1,4 @@
 # to be displayed in overview page
-recall_at_for_overview = 0.01
-score_name = "Recall {}%".format(recall_at_for_overview*100.0)
+precision_at_for_overview = 0.01
+score_name = "Precision at {}%".format(precision_at_for_overview*100.0)
 results_folder = "/Users/redshiftzero/Documents/Github/police-eis/results/"
