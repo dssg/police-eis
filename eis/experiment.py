36edefed475053d201b94874ca71a5705b1f55bb
redshiftzero <jen@redshiftzero.com>
2015-11-16 17:21:45 -0600

    Enable cycling through all feature sets (grouped by data source)

--- eis/experiment.py
+++ eis/experiment.py
@@ -8,6 +8,7 @@ import pickle
 import pdb
 import datetime
 from itertools import product
+import copy
 
 from eis import setup_environment, models, officer, dispatch, explore
 
@@ -32,52 +33,70 @@ def main(config_file_name="default.yaml"):
     except:
         log.exception("Failed to get experiment configuration file!")
 
-    if config["unit"] == "officer":
-        exp_data = officer.setup(config)
-    elif config["unit"] == "dispatch":
-        exp_data = dispatch.setup(config)
+    feature_groups = ["basic", "ia", "unit_div", "arrests",
+                      "citations", "incidents", "field_interviews",
+                      "cad", "training", "traffic_stops", "eis",
+                      "extraduty", "neighborhood"]
 
-    if config["make_feat_dists"] == True:
-        explore.make_all_dists(exp_data)
+    for group in feature_groups:
+        log.info("Running models without feature set {}!".format(
+          group))
+        # leave out features related to the selected group
+        features_to_use = {}
+        feature_groups_to_use = copy.copy(feature_groups)
+        feature_groups_to_use.remove(group)
+        for features in feature_groups_to_use:
+            features_to_use.update(config["features"][features])
 
-    log.info("Training data: {} rows. Testing data: {} rows.".format(
-        len(exp_data["train_y"]), len(exp_data["test_x"])))
+        this_config = copy.copy(config)
+        this_config["features"] = features_to_use
 
-    log.info("Running models on dataset...")
+        if config["unit"] == "officer":
+            exp_data = officer.setup(this_config)
+        elif config["unit"] == "dispatch":
+            exp_data = dispatch.setup(this_config)
 
-    parameter_names = sorted(config["parameters"])
-    parameter_values = [config["parameters"][p] for p in parameter_names]
-    all_params = product(*parameter_values)
+        if config["make_feat_dists"] == True:
+            explore.make_all_dists(exp_data)
 
-    for each_param in all_params:
-        timestamp = datetime.datetime.now().isoformat()
+        log.info("Training data: {} rows. Testing data: {} rows.".format(
+            len(exp_data["train_y"]), len(exp_data["test_x"])))
 
-        parameters = {name: value for name, value
-                      in zip(parameter_names, each_param)}
-        log.info("Training model: {} with {}".format(config["model"],
-                 parameters))
-        result_y, importances = models.run(exp_data["train_x"],
-                                           exp_data["train_y"],
-                                           exp_data["test_x"],
-                                           config["model"],
-                                           parameters)
+        log.info("Running models on dataset...")
 
-        config["parameters"] = parameters
-        log.info("Saving pickled results...")
-        to_save = {"test_labels": exp_data["test_y"],
-                   "test_predictions": result_y,
-                   "config": config,
-                   "features": exp_data["names"],
-                   "timestamp": timestamp,
-                   "parameters": parameters,
-                   "train_start_date": exp_data["train_start_date"],
-                   "test_end_date": exp_data["test_end_date"],
-                   "feature_importances": importances,
-                   "feature_importances_names": exp_data["train_x"].columns.values}
+        parameter_names = sorted(this_config["parameters"])
+        parameter_values = [this_config["parameters"][p] for p in parameter_names]
+        all_params = product(*parameter_values)
 
-        pkl_file = "{}{}_{}.pkl".format(
-            config['directory'], config['pkl_prefix'], timestamp)
-        pickle_results(pkl_file, to_save)
+        for each_param in all_params:
+            timestamp = datetime.datetime.now().isoformat()
+
+            parameters = {name: value for name, value
+                          in zip(parameter_names, each_param)}
+            log.info("Training model: {} with {}".format(this_config["model"],
+                     parameters))
+            result_y, importances = models.run(exp_data["train_x"],
+                                               exp_data["train_y"],
+                                               exp_data["test_x"],
+                                               this_config["model"],
+                                               parameters)
+
+            this_config["parameters"] = parameters
+            log.info("Saving pickled results...")
+            to_save = {"test_labels": exp_data["test_y"],
+                       "test_predictions": result_y,
+                       "config": this_config,
+                       "features": exp_data["names"],
+                       "timestamp": timestamp,
+                       "parameters": parameters,
+                       "train_start_date": exp_data["train_start_date"],
+                       "test_end_date": exp_data["test_end_date"],
+                       "feature_importances": importances,
+                       "feature_importances_names": exp_data["train_x"].columns.values}
+
+            pkl_file = "{}{}_{}.pkl".format(
+                this_config['directory'], this_config['pkl_prefix'], timestamp)
+            pickle_results(pkl_file, to_save)
 
     log.info("Done!")
     return None
