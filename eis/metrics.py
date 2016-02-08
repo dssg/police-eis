4665708a021e5de5866e867f815b5bbbc12c9ac8
redshiftzero <jen@redshiftzero.com>
2016-02-08 16:34:00 -0600

    Compute AUC in main modeling code

---
+++ eis/metrics.py
@@ -0,0 +1,10 @@
+import pdb
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+
+
+def compute_AUC(test_labels, test_predictions):
+    fpr, tpr, thresholds = metrics.roc_curve(
+        test_labels, test_predictions, pos_label=1)
+    return metrics.auc(fpr, tpr)
