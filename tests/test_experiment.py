bc068ab51a50d7b5eaf607326bfb73d9ba4358e1
redshiftzero <jen@redshiftzero.com>
2016-05-10 16:57:33 -0500

    Add tests for experiment.py

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,4 +1,36 @@
 from nose.tools import assert_equals
 
-def test_exp():
-    pass
+
+from eis import experiment
+
+
+config = {}
+
+class TestExperiment:
+    def test_date_array(self):
+        result = experiment.date_array(3600)
+        assert result[0].year == 2007
+
+
+    def test_generate_time_info_autogen_off(self):
+        config["fake_today"] = ["01May2009", "01May2010"]
+        config["training_window"] = [730, 1080]
+        config["prediction_window"] = [180, 365]
+        config["autogen_fake_todays"] = False
+        
+        expected_num_of_exps = len(config["fake_today"]) * \
+                               len(config["training_window"]) * \
+                               len(config["prediction_window"])
+        temporal_info = experiment.generate_time_info(config)
+
+        assert len(temporal_info) == expected_num_of_exps
+
+ 
+    def test_generate_time_info_autogen_on(self):
+        config["training_window"] = [730, 1080]
+        config["prediction_window"] = [180, 365]
+        config["autogen_fake_todays"] = True
+        
+        temporal_info = experiment.generate_time_info(config)
+
+        assert len(temporal_info) == 58 
