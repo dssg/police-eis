b87cb8863537e7756b4e9162ec2fa1764a105062
Henry Hinnefeld <henry.hinnefeld@gmail.com>
2016-07-21 16:57:27 +0000

    changed dispatch.setup() to expect python datetime object instead of str

--- eis/dispatch.py
+++ eis/dispatch.py
@@ -8,33 +8,73 @@ import datetime
 
 log = logging.getLogger(__name__)
 
+def run_traintest(config):
+    """Get training and testing datasets from the database"""
+    today_dt = datetime.datetime.strptime(config['fake_today', "%d%b%Y")
+    result = setup(config, today_dt)
+    return result
 
-def setup(config):
-    fake_today = datetime.datetime.strptime(config["fake_today"], "%d%b%Y")
-    train_start_date = datetime.datetime.strptime(config["fake_today"],
-                                                  "%d%b%Y") - \
-        datetime.timedelta(days=config["training_interval_days"])
-    test_end_date = datetime.datetime.strptime(config["fake_today"],
-                                               "%d%b%Y") + \
-        datetime.timedelta(days=config["testing_interval_days"])
+def setup(config, today):
+    """
+    Sets up dispatch-level experiment
 
-    log.info("Train window start: {}".format(train_start_date))
-    log.info("Train window stop: {}".format(fake_today))
-    log.info("Test window start: {}".format(fake_today))
-    log.info("Test window stop: {}".format(test_end_date))
+    Args:
+        config(dict): dict with information from the config file
+        today(datetime.datetime): python datetime object containing the date to 
+                                  split on for temporal cross-validation
+    """
 
-    log.info("Loading dispatch events and features to use as training...")
-    pass
+    train_start_date = today - datetime.timedelta(days=config["training_window"])
+    test_end_date = today + datetime.timedelta(days=config["prediction_window"])
 
-    log.info("Loading dispatch events and features to use as testing...")
-    pass
+    log.info("Train label window start: {}".format(train_start_date))
+    log.info("Train label window stop: {}".format(today))
+    log.info("Test label window start: {}".format(today))
+    log.info("Test label window stop: {}".format(test_end_date))
+
+    log.info("Loading dispatches and features to use as training...")
+    table_name = "features"
+    train_x, train_y, train_id, names = dataset.grab_officer_data(
+        config["features"],
+        train_start_date,
+        today,
+        train_start_date,
+        config["def_adverse"],
+        config["labelling"],
+        table_name)
+
+    # Testing data should include ALL officers, ignoring "noinvest" keyword
+    testing_labelling_config = config["labelling"].copy()
+    testing_labelling_config["noinvest"] = True
+
+    log.info("Loading officers and features to use as testing...")
+    test_x, test_y, test_id, names = dataset.grab_officer_data(
+        config["features"],
+        today,
+        test_end_date,
+        today,
+        config["def_adverse"],
+        testing_labelling_config,
+        table_name)
+
+    train_x_index = train_x.index
+    test_x_index = test_x.index
+    features = train_x.columns.values
+
+    # Feature scaling
+    scaler = preprocessing.StandardScaler().fit(train_x)
+    train_x = scaler.transform(train_x)
+    test_x = scaler.transform(test_x)
 
     return {"train_x": train_x,
             "train_y": train_y,
             "train_id": train_id,
             "test_x": test_x,
-            "test_y": test_y,
+            "test_y": test_y,  # For pilot test_y will not be used
             "test_id": test_id,
             "names": names,
             "train_start_date": train_start_date,
-            "test_end_date": test_end_date}
+            "test_end_date": test_end_date,
+            "train_x_index": train_x_index,
+            "test_x_index": test_x_index,
+            "features": features}
