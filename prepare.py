0800bc6c8b354a053f637296c51b4b401ddcd4e8
jmausolf <jgm346@gmail.com>
2016-07-28 23:19:22 +0000

    Cleaning up file

--- prepare.py
+++ prepare.py
@@ -10,7 +10,6 @@ import pickle
 sys.path.append("../")
 
 from eis import setup_environment
-#from setup_environment import *
 
 """
 Code to take top performing recent models and
@@ -31,6 +30,25 @@ except:
     pass
 
 
+def get_metric_best_models(timestamp, metric):
+
+    """
+    Get the evaluation results of the best recent models
+    by the specified timestamp and given metric
+    """
+
+    query   =  ("   SELECT {} FROM results.evaluations JOIN results.models \
+                    ON evaluations.model_id=models.model_id \
+                    WHERE run_time >= '{}' \
+                    AND {} is not null \
+                    ORDER BY {} DESC LIMIT 25; ").format(metric, timestamp, metric, metric)
+
+    df_models = pd.read_sql(query, con=con)
+    output = df_models[metric].apply(lambda x: str(x)).values
+    statement = "Resulting metric for models with best {} run on or after {}: \n".format(metric, timestamp)
+    print (statement, output)
+    return output
+
 
 def get_best_recent_models(timestamp, metric):
     """
@@ -46,18 +64,24 @@ def get_best_recent_models(timestamp, metric):
 
     df_models = pd.read_sql(query, con=con)
     output = df_models['run_time'].apply(lambda x: str(x).replace(' ', 'T')).values
-    #print (output)
     return output
 
-def pickle_results(pkl_file, to_save):
-    """
-    Save contents of experiment to pickle file for later use
-    """
 
-    with open(pkl_file, 'wb') as f:
-        pickle.dump(to_save, f, protocol=pickle.HIGHEST_PROTOCOL)
+def get_best_models(metric):
+    """
+    Grab the identifiers of the best performing (top AUC) models
+    from the database.
+    """
+    #query = "SELECT id_timestamp FROM models.full ORDER BY auc DESC LIMIT 25"
+    query   =  ("   SELECT run_time FROM results.evaluations JOIN results.models \
+                    ON evaluations.model_id=models.model_id \
+                    WHERE {} is not null \
+                    ORDER BY {} DESC LIMIT 25; ").format(metric, metric)
 
-    return None
+    df_models = pd.read_sql(query, con=con)
+    output = df_models['run_time'].apply(lambda x: str(x).replace(' ', 'T')).values
+    return output
+
 
 def get_pickel_best_models(timestamp, metric):
 
@@ -87,43 +111,6 @@ def get_pickel_best_models(timestamp, metric):
     return None
 
 
-def get_metric_best_models(timestamp, metric):
-
-    """
-    Get the evaluation results of the best recent models
-    by the specified timestamp and given metric
-    """
-
-    query   =  ("   SELECT {} FROM results.evaluations JOIN results.models \
-                    ON evaluations.model_id=models.model_id \
-                    WHERE run_time >= '{}' \
-                    AND {} is not null \
-                    ORDER BY {} DESC LIMIT 25; ").format(metric, timestamp, metric, metric)
-
-    df_models = pd.read_sql(query, con=con)
-    output = df_models[metric].apply(lambda x: str(x)).values
-    statement = "Resulting metric for models with best {} run on or after {}: \n".format(metric, timestamp)
-    print (statement, output)
-    return output
-
-
-def get_best_models(metric):
-    """
-    Grab the identifiers of the best performing (top AUC) models
-    from the database.
-    """
-    #query = "SELECT id_timestamp FROM models.full ORDER BY auc DESC LIMIT 25"
-    query   =  ("   SELECT run_time FROM results.evaluations JOIN results.models \
-                    ON evaluations.model_id=models.model_id \
-                    WHERE {} is not null \
-                    ORDER BY {} DESC LIMIT 25; ").format(metric, metric)
-
-    df_models = pd.read_sql(query, con=con)
-    output = df_models['run_time'].apply(lambda x: str(x).replace(' ', 'T')).values
-    #print(output)
-    return output
-
-
 def prepare_webapp_display(ids, src_dir, dest_dir):
     """
     Move the relevant webapp files into the directory that the evaluation
@@ -144,13 +131,5 @@ if __name__=='__main__':
     print("[*] Updating model list...")
     metrics = get_metric_best_models(args.timestamp, args.metric)
     print("[*] Dumping requested pickle files to results...")
-    ids = get_best_recent_models(args.timestamp, args.metric)
-    ids_all = get_best_models(args.metric)
     pickles = get_pickel_best_models(args.timestamp, args.metric)
     print("[*] Done!")
-
-    #pickle.load
-
-    #raw_outputs_dir = '/mnt/data4/jhelsby/newpilot/'
-    #webapp_display_dir = '/mnt/data4/jhelsby/currentdisplay/'
-    #prepare_webapp_display(ids, raw_outputs_dir, webapp_display_dir)
