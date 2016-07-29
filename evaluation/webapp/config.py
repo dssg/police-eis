6f27f6e5f0f12a2990c158ad6c3d0a433666b723
jmausolf <jgm346@gmail.com>
2016-07-29 15:34:03 +0000

    changing file path

--- evaluation/webapp/config.py
+++ evaluation/webapp/config.py
@@ -1,3 +1,16 @@
+import sys, os, glob
 # to be displayed in overview page
-#results_folder = "/Users/redshiftzero/Documents/Github/police-eis/results/"
-results_folder = "/Users/redshiftzero/Documents/Github/police-eis/results_all/"
+results_folder = "../../results/"
+
+
+def get_files(folder_name):
+
+	#Get Files in Folder
+	folder = str(folder_name)
+	os.chdir(folder)
+	pickle_files = glob.glob("*.pkl")
+
+	for pickle in pickle_files:
+		print ("Analyzing file ", pickle, "...")
+
+#get_files(results_folder)
