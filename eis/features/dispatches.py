6bf54635bdb88a725aef36a2401050e75b98d8de
t-davidson <trd54@cornell.edu>
2016-08-04 14:16:45 -0500

    fixed Henry's code I deleted - specific label classes

--- eis/features/dispatches.py
+++ eis/features/dispatches.py
@@ -16,21 +16,77 @@ time_format = "%Y-%m-%d %X"
 
 ### LABEL
 
-
-class Label(abstract.DispatchFeature):
+class LabelSustained(abstract.DispatchFeature):
     def __init__(self, **kwargs):
         abstract.DispatchFeature.__init__(self, **kwargs)
-        self.description = "Binary label, 1 if incident considered adverse occured during this dispatch"
+        self.description = "Binary label, 1 if this dispatch led to a sustained complaint"
+        self.is_label = True
         self.query = (  "SELECT "
                         "    dispatch_id, "
-                        "    case when sum(coalesce(incidents.number_of_unjustified_allegations, 0)) + "
-                        "    sum(coalesce(incidents.number_of_preventable_allegations, 0)) + "
-                        "    sum(coalesce(incidents.number_of_sustained_allegations, 0)) > 0 then 1 else 0 end as feature_column "
-                        "FROM (select * from staging.events_hub where event_datetime between '{}' and '{}' "
-                        "  and event_type_code = 4 and dispatch_id is not null ) as events_hub "
-                        "  left join staging.incidents as incidents "
-                        "  on events_hub.event_id = incidents.event_id "
-                        "GROUP by 1 ").format(self.from_date, self.to_date)
+                        "    CASE WHEN "
+                        "           SUM(COALESCE(incidents.number_of_sustained_allegations, 0)) > 0 "
+                        "         THEN 1 "
+                        "         ELSE 0 "
+                        "    END AS feature_column "
+                        "FROM "
+                        "   (SELECT * "
+                        "    FROM staging.events_hub "
+                        "    WHERE event_datetime BETWEEN '{}' AND '{}' "
+                        "    AND event_type_code = 4 "
+                        "    AND dispatch_id IS NOT NULL "
+                        "   ) AS events_hub "
+                        "LEFT JOIN staging.incidents AS incidents "
+                        "  ON events_hub.event_id = incidents.event_id "
+                        "GROUP BY 1 "
+                        .format(self.from_date, self.to_date))
+
+class LabelUnjustified(abstract.DispatchFeature):
+    def __init__(self, **kwargs):
+        abstract.DispatchFeature.__init__(self, **kwargs)
+        self.description = "Binary label, 1 if this dispatch led to an unjustified use of force"
+        self.is_label = True
+        self.query = (  "SELECT "
+                        "    dispatch_id, "
+                        "    CASE WHEN SUM(COALESCE(incidents.number_of_unjustified_allegations, 0)) > 0 "
+                        "         THEN 1 "
+                        "         ELSE 0 "
+                        "    END AS feature_column "
+                        "FROM "
+                        "   (SELECT * "
+                        "    FROM staging.events_hub "
+                        "    WHERE event_datetime BETWEEN '{}' AND '{}' "
+                        "    AND event_type_code = 4 "
+                        "    AND dispatch_id IS NOT NULL "
+                        "   ) AS events_hub "
+                        "LEFT JOIN staging.incidents AS incidents "
+                        "  ON events_hub.event_id = incidents.event_id "
+                        "GROUP BY 1 "
+                        .format(self.from_date, self.to_date))
+
+class LabelPreventable(abstract.DispatchFeature):
+    def __init__(self, **kwargs):
+        abstract.DispatchFeature.__init__(self, **kwargs)
+        self.description = "Binary label, 1 if this dispatch led to a preventable accidents"
+        self.is_label = True
+        self.query = (  "SELECT "
+                        "    dispatch_id, "
+                        "    CASE WHEN SUM(COALESCE(incidents.number_of_preventable_allegations, 0)) > 0 "
+                        "         THEN 1 "
+                        "         ELSE 0 "
+                        "    END AS feature_column "
+                        "FROM "
+                        "   (SELECT * "
+                        "    FROM staging.events_hub "
+                        "    WHERE event_datetime BETWEEN '{}' AND '{}' "
+                        "    AND event_type_code = 4 "
+                        "    AND dispatch_id IS NOT NULL "
+                        "   ) AS events_hub "
+                        "LEFT JOIN staging.incidents AS incidents "
+                        "  ON events_hub.event_id = incidents.event_id "
+                        "GROUP BY 1 "
+                        .format(self.from_date, self.to_date))
+
+
 
 
 #TODO
