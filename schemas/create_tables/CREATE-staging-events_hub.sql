ec0efa17ad1e73d4500e38fac350c895a40fbbd9
klaus <klaus.ackermann@monash.edu>
2016-10-13 23:42:48 -0500

    changed source id to int

--- schemas/create_tables/CREATE-staging-events_hub.sql
+++ schemas/create_tables/CREATE-staging-events_hub.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS staging.events_hub CASCADE;
 CREATE UNLOGGED TABLE staging.events_hub (
 	event_id                                                              serial primary key, -- unique id for the event
 	event_chain_id                                                        int,				  -- id for the set of events that are linked together
-	source_event_id 													  varchar,            -- unique id in the source table of this event. 
+	source_event_id 													  int,            -- unique id in the source table of this event.
 	department_defined_event_id											  varchar,            --    
 	officer_id                                                            int references staging.officers_hub(officer_id),                --
 	event_type_code                                                       int,                --
