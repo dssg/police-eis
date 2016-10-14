ce454ff78f833771574a95e046c9e29f061062bc
tweddielin <power6188@gmail.com>
2016-10-14 20:31:05 +0000

    Re-structure the current code

---
+++ webapp/webapp/query.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import json
+
+CONFIG_PATH = "/home/tlin/default_profile.json"
+with open(CONFIG_PATH) as f:
+    config = json.load(f)
+
+dbengine = create_engine('postgres://', connect_args=config)
+
+def get_best_models(timestamp, metric, parameter=None, number=25):
+
+    """
+    --------------------------------------------------------
+    Get the REPORT of the best models
+    by the specified timestamp and given metric
+
+    RETURNS RUN TIME, MODEL TYPE, METRIC, and VALUE
+    OR
+    RUN TIME, MODEL TYPE, METRIC, PARAMETER, and VALUE
+    --------------------------------------------------------
+    ARGUMENTS:
+        timestamp:  models run on or after given timestamp
+                    example: '2016-08-03'
+        metric:     metric to be optimized
+                    example: 'precision@'
+        parameter:  parameter value or threshold if any
+                    default=None
+                    example: '10.0'
+        number:     maximum number of desired results
+                    default = 25
+    --------------------------------------------------------
+    """
+    if parameter is None:
+        query = ("SELECT run_time, model_type, metric, parameter, value FROM results.evaluations JOIN results.models "
+                 "ON evaluations.model_id=models.model_id "
+                 "WHERE run_time >= '{}' "
+                 "AND value is not null "
+                 "AND metric = '{}' "
+                 "ORDER BY value DESC LIMIT {} ").format(timestamp, metric, number)
+
+    elif parameter is not None:
+        parameter = float(parameter)
+        metric = metric + "@"
+        query = ("SELECT run_time, model_type, metric, parameter, value FROM results.evaluations JOIN results.models "
+                 "ON evaluations.model_id=models.model_id "
+                 "WHERE run_time >= '{}' "
+                 "AND value is not null "
+                 "AND metric = '{}' "
+                 "AND parameter = '{}' "
+                 "ORDER BY value DESC LIMIT {} ; ").format(timestamp, metric, parameter, number)
+
+    df_models = pd.read_sql(query, con=dbengine)
+    output = df_models
+    return output
--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -4,13 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
 from webapp import app
+from webapp import query
 
-
-CONFIG_PATH = "/home/tlin/default_profile.json"
-with open(CONFIG_PATH) as f:
-    config = json.load(f)
-
-engine = create_engine('postgres://', connect_args=config)
 #DBSession = sessionmaker(bind=engine)
 #session = DBSession()
 
@@ -19,39 +14,21 @@ engine = create_engine('postgres://', connect_args=config)
 def index():
     return render_template('index.html')
 
-@app.route('/evaluations/search_ids')
-def search_ids():
-    timestamp = '2016-08-03'
-    metric = 'auc'
-    number = 5
-    query = ("      SELECT run_time FROM results.evaluations JOIN results.models \
-                    ON evaluations.model_id=models.model_id \
-                    WHERE run_time >= '{}' \
-                    AND value is not null \
-                    AND metric = '{}' \
-                    ORDER BY value DESC LIMIT {} ; ").format(timestamp, metric, number)
-    df_models = pd.read_sql(query, con=engine)
-    print(df_models)
-    #output = df_models['run_time'].apply(lambda x: str(x).replace(' ', 'T')).values
-    output = df_models
-    return render_template('index.html',tables=[output.to_html(classes='bestids')])
 
 @app.route('/evaluations/search_best_models', methods=['GET', 'POST'])
 def search_best_models():
     if request.method == 'POST':
-        metric = request.form['metric'] + "@"
-        parameter = float(request.form['parameter'])
-        number = request.form['number']
+        metric = request.form['metric']
+        if len(request.form['parameter']) == 0:
+            parameter = None
+        else:
+            parameter = request.form['parameter']
+
+        if len(request.form['number']) == 0:
+            number = 10
+        else:
+            number = request.form['number']
 
     timestamp = '2016-08-03'
-
-    query = ("      SELECT run_time, model_type, metric, parameter, value FROM results.evaluations JOIN results.models \
-                    ON evaluations.model_id=models.model_id \
-                    WHERE run_time >= '{}' \
-                    AND value is not null \
-                    AND metric = '{}' \
-                    AND parameter = '{}' \
-                    ORDER BY value DESC LIMIT {} ; ").format(timestamp, metric, parameter, number)
-    df_models = pd.read_sql(query, con=engine)
-    output = df_models
+    output = query.get_best_models(timestamp=timestamp, metric=metric, parameter=parameter, number=number)
     return render_template('index.html',tables=[output.to_html(classes='bestmodels')], number=number, parameter=parameter)
