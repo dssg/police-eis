8a95fa8827b8ee0ff315b063c464f1c193b789f9
klaus <klaus.ackermann@monash.edu>
2016-10-16 10:43:01 -0500

    added unique constraint for speed

--- schemas/create_tables/CREATE-staging-addresses.sql
+++ schemas/create_tables/CREATE-staging-addresses.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS staging.addresses; 
 CREATE UNLOGGED TABLE staging.addresses (
-	address_id                                                            serial,                --
-	department_defined_reference_id										  varchar,
+	address_id                                                            serial PRIMARY KEY ,                --
+	department_defined_reference_id										  varchar UNIQUE,
 	street_address                                                        text,               --street address 
 	second_street_address                                                 text,               --unit/apartment
 	city                                                                  text,               --city
