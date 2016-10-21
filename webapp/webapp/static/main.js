f3e5d382e80116b7dae042a821d9a9d3feb117c0
tweddielin <power6188@gmail.com>
2016-10-21 20:49:02 +0000

    Correct some spelling misakes and table font

--- webapp/webapp/static/main.js
+++ webapp/webapp/static/main.js
@@ -29,14 +29,14 @@ $(function() {
                 console.log(data.length);
                 // loop through results, append to dom
                 for (i = 0; i < data.length; i++) {
-                    $("#results").append('<tr><td><a href="'+'/evaluations/individual">'
+                    $("#results").append('<tr><th>'+(i+1)
+                                          +'</th><td><a href="'+'/evaluations/individual">'
                                           +data[i]['run_time']+'</a></td><td>'
                                           +data[i]['model_type']+'</td><td>'
                                           +data[i]['metric']+'</td><td>'
                                           +data[i]['parameter']+'</td><td>'
-                                          +data[i]['value']+'</tr>')
+                                          +data[i]['value'].toPrecision(4)+'</tr>')
                 };
-
             }
         });
         });
