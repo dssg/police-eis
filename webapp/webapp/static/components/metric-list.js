7ae7c8bb38cc86147e0a942fa70210d57594eaff
tweddielin <power6188@gmail.com>
2016-11-21 19:38:54 +0000

    fix indentation

--- webapp/webapp/static/components/metric-list.js
+++ webapp/webapp/static/components/metric-list.js
@@ -4,41 +4,41 @@ var MetricList = React.createClass({
     },
     addMetric: function() {
         this.setState(
-			{'metrics': this.state.metrics.concat([this.state.metrics.length]) }
-		);
+            {'metrics': this.state.metrics.concat([this.state.metrics.length]) }
+        );
     },
-	removeMetric: function(metricId) {
-		var newMetrics = this.state.metrics.filter(function(item) {
-			return item !== metricId;
-		});
-		this.setState({ 'metrics': newMetrics });
-	},
+    removeMetric: function(metricId) {
+        var newMetrics = this.state.metrics.filter(function(item) {
+            return item !== metricId;
+        });
+        this.setState({ 'metrics': newMetrics });
+    },
     render: function() {
-		var self = this;
+        var self = this;
         return (
-			<span>
-				<div className="row">
-					Show models with
-				</div>
-				<div className="row">
-				  {this.state.metrics.map(function(metricId) {
-					return <MetricSelector
-							key={metricId}
-							index={metricId}
-							handleDeleteClick={self.removeMetric} />;
-				  })}
-				</div>
-				<div className="row">
-					<button
-						type="button"
-						onClick={this.addMetric}
-						style={ { padding: '5px 10px' } }
-						className="btn btn-info btn-xs"
-					>
-						Add
-					</button>
-				</div>
-			</span>
+            <span>
+                <div className="row">
+                    Show models with
+                </div>
+                <div className="row">
+                  {this.state.metrics.map(function(metricId) {
+                    return <MetricSelector
+                            key={metricId}
+                            index={metricId}
+                            handleDeleteClick={self.removeMetric} />;
+                  })}
+                </div>
+                <div className="row">
+                    <button
+                        type="button"
+                        onClick={this.addMetric}
+                        style={ { padding: '5px 10px' } }
+                        className="btn btn-info btn-xs"
+                    >
+                        Add
+                    </button>
+                </div>
+            </span>
         )
     }
 });
--- webapp/webapp/static/components/metric-selector.js
+++ webapp/webapp/static/components/metric-selector.js
@@ -1,40 +1,40 @@
 var MetricSelector = React.createClass({
-	choices: [
-			"precision",
-			"recall",
-			"auc",
-			"f1",
-			"true positives",
-			"true negatives",
-			"false positives",
-			"false negatives"
-	],
-	inputName: function() {
-		return "parameter" + this.props.index.toString();
-	},
-	selectName: function() {
-		return "metric" + this.props.index.toString();
-	},
-	handleDelete: function() {
-		this.props.handleDeleteClick(this.props.index);
-	},
-	render: function() {
-		return (
-			<div>
-				<select name={this.selectName()}>
-				{this.choices.map(function(choice){
-					return <option value={choice}>{choice}</option>;
-				})}
-				</select>
-				<span style={ { margin: '0 1em 0 1em' }}>@</span>
-				<input type='text' name={this.inputName()} size="3" /> %
-				<button
-					className='btn btn-xs btn-danger'
-					type='button'
-					onClick={this.handleDelete}>
-					X
-				</button>
-			</div>
-		);
-	}
+    choices: [
+            "precision",
+            "recall",
+            "auc",
+            "f1",
+            "true positives",
+            "true negatives",
+            "false positives",
+            "false negatives"
+    ],
+    inputName: function() {
+        return "parameter" + this.props.index.toString();
+    },
+    selectName: function() {
+        return "metric" + this.props.index.toString();
+    },
+    handleDelete: function() {
+        this.props.handleDeleteClick(this.props.index);
+    },
+    render: function() {
+        return (
+            <div>
+                <select name={this.selectName()}>
+                {this.choices.map(function(choice){
+                    return <option value={choice}>{choice}</option>;
+                })}
+                </select>
+                <span style={ { margin: '0 1em 0 1em' }}>@</span>
+                <input type='text' name={this.inputName()} size="3" /> %
+                <button
+                    className='btn btn-xs btn-danger'
+                    type='button'
+                    onClick={this.handleDelete}>
+                    X
+                </button>
+            </div>
+        );
+    }
 });
