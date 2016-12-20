9801e15e53268095a9153f3e0ade741bfc9e0e32
Tristan Crockett <tristan.h.crockett@gmail.com>
2016-12-20 16:30:55 -0600

    Remove webapp

--- evaluation/webapp/config.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# this config file sets the current directory for a given machine without hardcoding the path
-import os
-cwd = os.getcwd()
-results_folder = str(cwd.replace('evaluation', 'results/')).replace("'",'"')
