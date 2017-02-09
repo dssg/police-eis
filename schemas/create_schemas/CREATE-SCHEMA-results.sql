aa5b43247de45de38d064f8f995f5c83263d5dda
klaus <klaus.ackermann@monash.edu>
2017-02-09 12:21:24 -0600

    updated result schema with indexes

--- schemas/create_schemas/CREATE-SCHEMA-results.sql
+++ schemas/create_schemas/CREATE-SCHEMA-results.sql
@@ -13,47 +13,49 @@ CREATE TABLE results.model_groups
 
 -- model table containing each of the models run.
 CREATE TABLE results.models (
-  model_id              SERIAL PRIMARY KEY,
-  model_group_id        INT REFERENCES results.model_groups (model_group_id),
-  run_time              TIMESTAMP,
-  batch_run_time        TIMESTAMP,
-  model_type            TEXT,
-  model_parameters      JSONB,
-  model_comment         TEXT,
-  batch_comment         TEXT,
-  config                JSONB,
-  test                  BOOL,
-  model_hash            varchar(20),
-  train_matrix_uuid     varchar(36)
+  model_id          SERIAL PRIMARY KEY,
+  model_group_id    INT REFERENCES results.model_groups (model_group_id),
+  run_time          TIMESTAMP,
+  batch_run_time    TIMESTAMP,
+  model_type        TEXT,
+  model_parameters  JSONB,
+  model_comment     TEXT,
+  batch_comment     TEXT,
+  config            JSONB,
+  test              BOOL,
+  model_hash        VARCHAR(20) UNIQUE,
+  train_matrix_uuid VARCHAR(36)
 );
 
 -- predictions corresponding to each model.
 CREATE TABLE results.predictions (
-  model_id     INT REFERENCES results.models (model_id),
-  as_of_date   TIMESTAMP,
-  entity_id    BIGINT,
-  score        NUMERIC,
-  label_value  INT,
-  rank_abs     INT,
-  rank_pct     REAL,
-  matrix_uuid  varchar(36)
+  model_id    INT REFERENCES results.models (model_id),
+  as_of_date  TIMESTAMP,
+  entity_id   BIGINT,
+  score       NUMERIC,
+  label_value INT,
+  rank_abs    INT,
+  rank_pct    REAL,
+  matrix_uuid VARCHAR(36)
 );
+
+CREATE INDEX ON results.predictions (model_id);
+CREATE INDEX ON results.predictions (as_of_date);
+CREATE INDEX ON results.predictions (model_id, as_of_date);
 
 -- evaluation table containing metrics for each of the models run.
 CREATE TABLE results.evaluations (
-  model_id     INT REFERENCES results.models (model_id),
-  metric       TEXT,
-  parameter    TEXT,
-  value        NUMERIC,
-  comment      TEXT,
-  as_of_date   TIMESTAMP
+  model_id   INT REFERENCES results.models (model_id),
+  metric     TEXT,
+  parameter  TEXT,
+  value      NUMERIC,
+  comment    TEXT,
+  as_of_date TIMESTAMP
 );
 
--- data table for storing pickle blobs.
-CREATE TABLE results.data (
-  model_id    INT REFERENCES results.models (model_id),
-  pickle_blob BYTEA
-);
+CREATE INDEX ON results.evaluations (model_id);
+CREATE INDEX ON results.evaluations (as_of_date);
+CREATE INDEX ON results.evaluations (model_id, as_of_date);
 
 -- feature_importance table for storing a json with feature importances
 CREATE TABLE results.feature_importances (
@@ -64,13 +66,18 @@ CREATE TABLE results.feature_importances (
   rank_pct           REAL
 );
 
+CREATE INDEX ON results.feature_importances (model_id);
+
 -- individual feature importance
 CREATE TABLE results.individual_importances (
-  model_id INT REFERENCES results.models (model_id),
-  unit_id  BIGINT,
-  risk_1   TEXT,
-  risk_2   TEXT,
-  risk_3   TEXT,
-  risk_4   TEXT,
-  risk_5   TEXT
+  model_id  INT REFERENCES results.models (model_id),
+  entity_id BIGINT,
+  risk_1    TEXT,
+  risk_2    TEXT,
+  risk_3    TEXT,
+  risk_4    TEXT,
+  risk_5    TEXT
 );
+
+CREATE INDEX ON results.individual_importances (model_id);
+
