ca4dac9b13d8b200793cb52d838fcc910205d01b
andreanr <andrea.navarrete126@gmail.com>
2017-02-14 21:47:43 +0000

    change to entity id

--- eis/dataset.py
+++ eis/dataset.py
@@ -156,8 +156,8 @@ def store_prediction_info( timestamp, unit_id_train, unit_id_test, unit_predicti
     # append data into predictions table. there is probably a faster way to do this than put it into a
     # dataframe and then use .to_sql but this works for now.
     dataframe_for_insert = pd.DataFrame( {  "model_id": this_model_id,
-                                            "unit_id": unit_id_test,
-                                            "unit_score": unit_predictions,
+                                            "entity_id": unit_id_test,
+                                            "score": unit_predictions,
                                             "label_value": unit_labels } )
 
     if store_as_csv:
