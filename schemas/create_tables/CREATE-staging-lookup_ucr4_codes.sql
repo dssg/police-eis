5c2054cfeace17c6e86069d7489f31573c7f322f
andreanr <andrea.navarrete126@gmail.com>
2017-03-21 21:05:13 +0000

    add ucr4 code grouped matching dispatch types, add lookuptables for ucr4 grouped and codes

---
+++ schemas/create_tables/CREATE-staging-lookup_ucr4_codes.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS staging.lookup_ucr4_codes;
+CREATE  TABLE staging.lookup_ucr4_codes (
+        code                                                                  varchar,                --
+        value                                                                 varchar,
+        description                                                           varchar             --
+);
---
+++ schemas/create_tables/CREATE-staging-lookup_ucr4_grouped_dispatch_types.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS staging.lookup_ucr4_grouped_dispatch_types;
+CREATE  TABLE staging.lookup_ucr4_grouped_dispatch_types (
+        code                                                                  int,                --
+        value                                                                 varchar,            --
+        description                                                           varchar             --
+);
