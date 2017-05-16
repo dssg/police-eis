7c9ec8a4d061467a92f752626851c7f9afea5dd0
OpsWorks user bkuester <bkt@uchicago.edu>
2017-05-16 21:17:54 +0000

    add necessary constraint to lookup_intervention_types, remove redundant sql file

--- schemas/create_tables/CREATE-staging-lookup_intervention_type.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS staging.lookup_intervention_type; 
-CREATE  TABLE staging.lookup_intervention_type (
-	code                                                                  int,                --
-	value                                                                 varchar,            --
-	description                                                           varchar             --
-);
--- schemas/create_tables/CREATE-staging-lookup_intervention_types.sql
+++ schemas/create_tables/CREATE-staging-lookup_intervention_types.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS staging.lookup_intervention_types; 
 CREATE  TABLE staging.lookup_intervention_types (
-	code                                                                  int,                --
+	code                                                                  int primary key,                --
 	value                                                                 varchar,            --
 	description                                                           varchar             --
 );
