bc6fa1ddda7af10de10ad52931713e50e0a6f0e6
harnav <hareemnav@gmail.com>
2017-06-16 16:25:04 -0500

    updating the run file with the production setup

--- eis/run.py
+++ eis/run.py
@@ -19,7 +19,7 @@ from . import populate_features, populate_labels
 from . import utils
 from .run_models import RunModels
 from triage.utils import save_experiment_and_get_hash 
-
+from dateutil.relativedelta import relativedelta
 log = logging.getLogger(__name__)
 
 def main(config_file_name, labels_config_file, args):
@@ -35,17 +35,91 @@ def main(config_file_name, labels_config_file, args):
     config = utils.read_yaml(config_file_name)
     labels_config = utils.read_yaml(labels_config_file)
 
+    # If you specify production in the args
+    if args.production:
+
+        print(args.model)
+        print(args.date)
+
+        db_engine = setup_environment.get_database()
+
+        # Create labels table
+        populate_labels.create_labels_table(config, config['officer_label_table_name'])
+
+        # Write query to pull all information, write to a new config to pass to populate_features
+        config_query = '''SELECT config->'feature_blocks',
+          config->'unit',
+          config->'temporal_info',
+          config->'officer_features'
+          from results.models where model_group_id = {};'''.format(args.model)
+
+        config_df = pd.read_sql(config_query, db_engine)
+
+        prod_config = {'feature_blocks': config_df.iloc[0, 0], 'unit': config_df.iloc[0, 1],
+            'temporal_info':config_df.iloc[0, 2], 'officer_features':config_df.iloc[0, 3]}
+
+        # Load in arguments, convert to appropriate time deltas
+        pred_wind = prod_config['temporal_info']['prediction_window']
+        pred_wind_dict = utils.relative_deltas_conditions(pred_wind)
+        pred_wind_delta = relativedelta(pred_wind_dict[pred_wind[0]])
+
+        train_size = prod_config['temporal_info']['train_size']
+        train_size_dict = utils.relative_deltas_conditions(train_size)
+        train_size_delta = relativedelta(train_size_dict[train_size[0]])
+
+        prod_config['temporal_info']['train_start_date'] = pd.to_datetime(args.date) - pred_wind_delta - train_size_delta
+        prod_config['temporal_info']['update_window'] = ['10y']
+        prod_config['temporal_info']['test_time_ahead'] = ['0d']
+
+        # Specify number of cpus and schema feature blocks
+        s = {'schema_feature_blocks': 'production_test'}
+        prod_config.update(s)
+        cpu = {'n_cpus': 1}
+        prod_config.update(cpu)
+
+        # To generate matrices need this info
+        temporal_sets = utils.generate_temporal_info(prod_config['temporal_info'])
+        block_sets = utils.feature_blocks_sets(prod_config['officer_features'], 0)
+        grid_config = utils.generate_model_config(config)
+
+        # More info
+        misc_db_parameters = {'config': prod_config,
+                              'test': config['test_flag'],
+                              'model_comment': config['model_comment'],
+                              'batch_comment': config['batch_comment']
+                              }
+
+        models_args = {'labels': config['labels'],
+                       'features': prod_config['feature_blocks'],
+                       'schema_name': prod_config["schema_feature_blocks"],
+                       'feature_lookback_duration': prod_config['temporal_info']['timegated_feature_lookback_duration'],
+                       'labels_config': labels_config,
+                       'labels_table_name': config['officer_label_table_name'],
+                           #config['officer_label_table_name'],
+                       'grid_config': grid_config,
+                       'project_path': config['project_path'],
+                       'misc_db_parameters': misc_db_parameters}
+
+        populate_features.populate_features_table(prod_config, prod_config['schema_feature_blocks'])
+        populate_labels.populate_labels_table(config, labels_config, config['officer_label_table_name'])
+
+        Parallel(n_jobs=1, verbose=51)(delayed(generate_all_matrices)(temporal_set, blocks, **models_args)
+                                            for temporal_set, blocks in product(temporal_sets, block_sets))
+
+        log.info('Done creating all matrices')
+        sys.exit()
+
     # If asked to generate features, then do that and stop.
     if args.buildfeatures:
 
         log.info("Re-building features...")
 
         # Create the features and labels table.
-        populate_labels.create_labels_table(config, config['officer_label_table_name'])
+        #populate_labels.create_labels_table(config, config['officer_label_table_name'])
 
         # Populate the featuress  and labels table
-        populate_features.populate_features_table(config, config["schema_feature_blocks"])
-        populate_labels.populate_labels_table(config, labels_config, config['officer_label_table_name'])
+        #populate_features.populate_features_table(config, config["schema_feature_blocks"])
+        #populate_labels.populate_labels_table(config, labels_config, config['officer_label_table_name'])
 
         log.info('Done creating features table')
         sys.exit()
@@ -77,6 +151,7 @@ def main(config_file_name, labels_config_file, args):
 
     n_cups = config['n_cpus']
 
+
     if args.generatematrices:
         # Parallelization
         Parallel(n_jobs=n_cups, verbose=51)(delayed(generate_all_matrices)(temporal_set, blocks,  **models_args)
@@ -84,6 +159,7 @@ def main(config_file_name, labels_config_file, args):
 
         log.info('Done creating all matrices')
         sys.exit() 
+
 
     # Run models
     db_engine = setup_environment.get_database()
@@ -164,6 +240,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, help="pass your config", default="default.yaml")
     parser.add_argument("--labels", type=str, help="pass your labels config", default="labels.yaml")
+    parser.add_argument("--date", type=str, help="pass date")
+    parser.add_argument("--model", type=str, help="specify model id")
+    parser.add_argument("-p", "--production", help="populate the production schemas", action='store_true')
     parser.add_argument("-b", "--buildfeatures", help="build the features and stop", action='store_true')
     parser.add_argument("-m", "--generatematrices", help="build all matrices used for running models", action='store_true')
     args = parser.parse_args()
