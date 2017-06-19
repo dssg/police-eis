752b0985a1e64df11892b062182b7072f380db54
OpsWorks user jwalsh <jwalsh@dsapp-super-master.localdomain>
2017-06-19 06:50:30 +0000

    readable feature names

--- eis/populate_production_schema.py
+++ eis/populate_production_schema.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import yaml
+import json
 import logging
 import sys
 import argparse
@@ -50,6 +51,15 @@ def main(chosen_model_group_id, matrix_location):
 
 
 
+def get_query(feature_name):
+    ## Load the features_config
+    with open('eis/features/features_descriptions.yaml') as f:
+        features_config = yaml.load(f)
+    query = """SELECT * from public.get_feature_complete_description('{feature}',
+        '{feature_names}'::JSON, '{time_aggregations}'::JSON, '{metrics}'::JSON)""".format(feature=feature_name,feature_names=json.dumps(features_config['feature_names']), time_aggregations = json.dumps(features_config['time_aggregations']), metrics = json.dumps(features_config['metrics_name']))
+    return query
+
+
 class populate_production_schema(object):
 
     def __init__(self, db_engine, chosen_model_group_id, matrix_location):
@@ -93,9 +103,42 @@ class populate_production_schema(object):
         """
 
         db_conn = self.db_engine.raw_connection()
+
         query = "select production.populate_individual_importances({:d});".format(self.chosen_model_group_id)
         db_conn.cursor().execute(query)
         db_conn.commit()
+
+        ## Get the feature names you are trying to map
+        feature_query = "SELECT distinct risk_1 as feature from production.individual_importances order by risk_1;"
+        feature_names = pd.read_sql(feature_query, db_conn)
+
+        list_of_dfs = []
+        for i in range(len(feature_names)):
+            list_of_dfs.append(pd.read_sql(get_query(feature_names.feature[i]), db_conn))
+
+        ## Concat the dfs into one df
+        feature_mapping = pd.concat(list_of_dfs, axis=0, ignore_index=True)
+        feature_mapping['column_new_name'] = feature_mapping['metric_name'] + ' ' + feature_mapping['feature_long_name'] + ' ' + feature_mapping['of_type'] + ' ' + feature_mapping['time_aggregation']
+        feature_mapping = feature_mapping.drop(['metric_name', 'feature_long_name', 'of_type', 'time_aggregation'], axis=1)
+
+        ## replace old values with new values
+        individual_importances = pd.read_sql('select * from production.individual_importances;', db_conn)
+        individual_importances1 = pd.merge(individual_importances[['model_id', 'as_of_date','risk_1']], feature_mapping, left_on='risk_1', right_on='column_original_name', how='left')
+        individual_importances2 = pd.merge(individual_importances[['model_id', 'as_of_date','risk_2']], feature_mapping, left_on='risk_2', right_on='column_original_name', how='left')
+        individual_importances3 = pd.merge(individual_importances[['model_id', 'as_of_date','risk_3']], feature_mapping, left_on='risk_3', right_on='column_original_name', how='left')
+        individual_importances4 = pd.merge(individual_importances[['model_id', 'as_of_date','risk_4']], feature_mapping, left_on='risk_4', right_on='column_original_name', how='left')
+        individual_importances5 = pd.merge(individual_importances[['model_id', 'as_of_date','risk_5']], feature_mapping, left_on='risk_5', right_on='column_original_name', how='left')
+        
+        individual_importances = pd.merge(individual_importances1, individual_importances2, on=['model_id', 'as_of_date'])
+        individual_importances = pd.merge(individual_importances, individual_importances3, on=['model_id', 'as_of_date'])
+        individual_importances = pd.merge(individual_importances, individual_importances4, on=['model_id', 'as_of_date'])
+        individual_importances = pd.merge(individual_importances, individual_importances5, on=['model_id', 'as_of_date'])
+
+
+        ## Write to csv
+        individual_importances.to_sql('production.individual_importances', db_conn, if_exists='replace')
+        
+
         db_conn.close()
 
 
