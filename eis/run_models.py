32b5a50cd4502315e19bf019d398dde0ff4cd0fc
klaus <klaus.ackermann@monash.edu>
2017-06-23 18:08:58 -0500

    added induvidal feature importance ranking based on the top30 features of a tree, ranked by the indivudial officer feature distribution

--- eis/run_models.py
+++ eis/run_models.py
@@ -5,6 +5,7 @@ import pdb
 import datetime
 import logging
 import json
+import numpy as np
 
 from flufl.lock import Lock
 
@@ -61,7 +62,6 @@ class RunModels():
         for l1 in self.labels:
             for l2 in l1:
                 self.labels_config[l2] = labels_config[l2]
-        
 
         # feature loader
         self.feature_loader = FeatureLoader(self.features,
@@ -77,11 +77,10 @@ class RunModels():
                                             )
         self.features_list = self.feature_loader.features_list()
 
-    def dt_handler(self,x):
+    def dt_handler(self, x):
         if isinstance(x, datetime.datetime) or isinstance(x, datetime.date):
             return x.isoformat()
         raise TypeError("Unknown type")
-
 
     def load_store_matrix(self, metadata, as_of_dates, return_matrix=True):
         """
@@ -98,11 +97,11 @@ class RunModels():
             if os.path.isfile(matrix_filename + '.h5'):
                 log.debug(' Matrix {} already stored'.format(uuid))
                 if return_matrix:
-                    df = metta.metta_io.recover_matrix(metadata,self.matrices_path)
+                    df = metta.metta_io.recover_matrix(metadata, self.matrices_path)
                     return df, uuid
 
             else:
-                
+
                 df = self.feature_loader.get_dataset(as_of_dates)
                 log.debug('storing matrix {}'.format(uuid))
                 metta.metta_io.archive_matrix(matrix_config=metadata,
@@ -120,7 +119,7 @@ class RunModels():
             'train_size': self.temporal_split['train_size'],
             'features_frequency': self.temporal_split['features_frequency'],
             'blocks': sorted(self.blocks)
-          }
+        }
 
         matrix_metadata = {
             # temporal information
@@ -148,42 +147,42 @@ class RunModels():
         for i in range(0, len(list_to_sort)):
             min = i
             for j in range(i + 1, len(list_to_sort)):
-                if isinstance(list_to_sort[j], (tuple,dict)):
+                if isinstance(list_to_sort[j], (tuple, dict)):
                     if sorted(list_to_sort[j])[0] < list_to_sort[min]:
                         min = j
-                elif isinstance(list_to_sort[min], (tuple,dict)):
+                elif isinstance(list_to_sort[min], (tuple, dict)):
                     if list_to_sort[j] < sorted(list_to_sort[min])[0]:
                         min = j
                 else:
                     if list_to_sort[j] < list_to_sort[min]:
                         min = j
-            list_to_sort[i], list_to_sort[min] = list_to_sort[min], list_to_sort[i] 
-            
+            list_to_sort[i], list_to_sort[min] = list_to_sort[min], list_to_sort[i]
+
         return list_to_sort
 
     def _make_hashable(self, o):
-            if isinstance(o, (tuple,list)):
-                l = []
-                for e in o:
+        if isinstance(o, (tuple, list)):
+            l = []
+            for e in o:
 
-                    if isinstance(e, (str, int)):
-                        l.append(str(e))
+                if isinstance(e, (str, int)):
+                    l.append(str(e))
 
-                    elif isinstance(e, (dict)):
-                        l.append(self._make_hashable(e))
+                elif isinstance(e, (dict)):
+                    l.append(self._make_hashable(e))
 
-                    else:
-                        l.append(e)
+                else:
+                    l.append(e)
 
-                return self.__sorting_multiple_types(l)
-                
-            if isinstance(o, dict):
-                return {k:self._make_hashable(o[k]) for k in sorted(o)}
-    
-            if isinstance(o, (set, frozenset)):
-                return list(sorted(self._make_hashable(e) for e in o))
-            
-            return o
+            return self.__sorting_multiple_types(l)
+
+        if isinstance(o, dict):
+            return {k: self._make_hashable(o[k]) for k in sorted(o)}
+
+        if isinstance(o, (set, frozenset)):
+            return list(sorted(self._make_hashable(e) for e in o))
+
+        return o
 
     def generate_matrices(self):
 
@@ -205,8 +204,8 @@ class RunModels():
             # Load and store matrixes
             log.info('Load test matrix for as of date: {}'.format(test_date))
             test_matrix_id = str([test_date,
-                                self.labels,
-                               self.temporal_split['prediction_window']])
+                                  self.labels,
+                                  self.temporal_split['prediction_window']])
 
             test_metadata = self._make_metadata(
                 datetime.datetime.strptime(test_date, "%Y-%m-%d"),
@@ -217,7 +216,7 @@ class RunModels():
 
             self.load_store_matrix(test_metadata, [test_date], return_matrix=False)
 
-    def setup_train_models(self,model_storage):
+    def setup_train_models(self, model_storage):
         train_matrix_id = str([sorted(self.temporal_split['train_as_of_dates']),
                                self.labels,
                                self.temporal_split['prediction_window']])
@@ -230,12 +229,12 @@ class RunModels():
             self.temporal_split['train_as_of_dates']
         )
 
-
         # Inlcude metadata in config for db
-        self.misc_db_parameters['config']['train_metadata'] = json.dumps(train_metadata, default=self.dt_handler, sort_keys=True)
-        self.misc_db_parameters['config']['labels_config']= self.labels_config
-        #self.misc_db_parameters['config'] = json.dump(self.misc_db_parameters['config'],default=self.dt_handler, sort_keys=True)
-        
+        self.misc_db_parameters['config']['train_metadata'] = json.dumps(train_metadata, default=self.dt_handler,
+                                                                         sort_keys=True)
+        self.misc_db_parameters['config']['labels_config'] = self.labels_config
+        # self.misc_db_parameters['config'] = json.dump(self.misc_db_parameters['config'],default=self.dt_handler, sort_keys=True)
+
         # Load train matrix
         log.info('Load train matrix using as of dates: {}'.format(self.temporal_split['train_as_of_dates']))
         train_df, train_matrix_uuid = self.load_store_matrix(train_metadata, self.temporal_split['train_as_of_dates'])
@@ -245,7 +244,6 @@ class RunModels():
                         unique value, no point in training this model. Skipping
                         ''', train_matrix_uuid)
             return None, None
-
 
         # remove the index from the data-frame
         for column in train_metadata['indices']:
@@ -257,9 +255,6 @@ class RunModels():
         # add to parameters to store in db
         self.misc_db_parameters['train_matrix_uuid'] = train_matrix_uuid
         train_matrix_store = InMemoryMatrixStore(train_df.iloc[:, :-1], train_metadata, train_df.iloc[:, -1])
-
-
-
 
         trainer = ModelTrainer(project_path=self.project_path,
                                experiment_hash=self.experiment_hash,
@@ -276,7 +271,6 @@ class RunModels():
 
     def train_test_models(self, train_matrix_uuid, model_ids_generator, model_storage):
 
-
         predictor = Predictor(project_path=self.project_path,
                               model_storage_engine=model_storage,
                               db_engine=self.db_engine)
@@ -284,7 +278,6 @@ class RunModels():
         for trained_model_id in model_ids_generator:
             ## Prediction
             log.info('Predict for model_id: {}'.format(trained_model_id))
-
 
             # Loop over testing as of dates
             for test_date in self.temporal_split['test_as_of_dates']:
@@ -322,7 +315,13 @@ class RunModels():
                                 ''', test_uuid)
                 else:
                     log.info('Generate Evaluations for model_id: {}'.format(trained_model_id))
-                    self.evaluations(predictions_proba, predictions_binary, test_df.iloc[:, -1], trained_model_id, test_date)
+                    self.evaluations(predictions_proba, predictions_binary, test_df.iloc[:, -1], trained_model_id,
+                                     test_date)
+                    self.individual_feature_ranking(
+                        fitted_model=predictor.load_model(trained_model_id),
+                        test_matrix=test_df.iloc[:, :-1],
+                        model_id=trained_model_id,
+                        test_date=test_date)
 
             # remove trained model from memory
             predictor.delete_model(trained_model_id)
@@ -332,16 +331,13 @@ class RunModels():
     # this function is used for training and scoring a day
     def train_score_models(self, model_ids_generator, model_storage):
 
-
         predictor = Predictor(project_path=self.project_path,
                               model_storage_engine=model_storage,
                               db_engine=self.db_engine)
 
-
         for trained_model_id in model_ids_generator:
             ## Prediction
             log.info('Predict for model_id: {}'.format(trained_model_id))
-
 
             # Loop over testing as of dates
             for test_date in self.temporal_split['test_as_of_dates']:
@@ -372,8 +368,8 @@ class RunModels():
                 predictions_binary, predictions_proba = predictor.predict(trained_model_id, test_matrix_store,
                                                                           misc_db_parameters)
                 ## Evaluation
-                #log.info('Generate Evaluations for model_id: {}'.format(trained_model_id))
-                #self.evaluations(predictions_proba, predictions_binary, test_df.iloc[:, -1], trained_model_id, test_date)
+                # log.info('Generate Evaluations for model_id: {}'.format(trained_model_id))
+                # self.evaluations(predictions_proba, predictions_binary, test_df.iloc[:, -1], trained_model_id, test_date)
 
             # remove trained model from memory
             predictor.delete_model(trained_model_id)
@@ -387,7 +383,8 @@ class RunModels():
         db_conn = self.db_engine.raw_connection()
 
         # remove all existing evaluations before re-writing
-        query = "DELETE FROM results.evaluations where model_id = {} and evaluation_start_time = '{}'::TIMESTAMP ".format(model_id,test_date)
+        query = "DELETE FROM results.evaluations where model_id = {} and evaluation_start_time = '{}'::TIMESTAMP ".format(
+            model_id, test_date)
         db_conn.cursor().execute(query)
         db_conn.commit()
 
@@ -411,6 +408,75 @@ class RunModels():
             dataset.store_evaluation_metrics(model_id, evaluation, metric, test_date, db_conn, metric_parameter,
                                              comment)
         db_conn.close()
+        return None
+
+    def individual_feature_ranking(self, fitted_model, test_matrix, model_id, test_date):
+        ###################
+        # This method is a beta version tested for top k optimized random forests
+        ###################
+
+        # get the list of all features in the test matrix
+        feature_list = test_matrix.columns
+
+        # extract all global feature importance values from the tree
+        importance_dict = {}
+        for j, value in enumerate(fitted_model.feature_importances_):
+            if value > 0:
+                importance_dict[feature_list[j]] = value
+
+        # filter out dummies
+        importance_dict_filtered = dict((k, v) for k, v in importance_dict.items() if not 'dummy' in k)
+
+        rftree_feature_list = sorted(importance_dict_filtered, key=importance_dict_filtered.get, reverse=True)
+
+        # create a new text matrix with float conversion (panda stores some columns from postgres as decimal object)
+        tmp_test = test_matrix.applymap(lambda x: float(x))
+        test_matrix_reduced = pd.DataFrame()
+
+        # add the top 30 features from the RandomForest used without dummies
+        for idx, feature in enumerate(rftree_feature_list):
+            if idx >= 30:
+                break
+            test_matrix_reduced[feature] = tmp_test[feature]
+
+        test_matrix_rank_distance = pd.DataFrame()
+
+        for feature in test_matrix_reduced.columns:
+            feature_median = test_matrix_reduced[feature].median()
+            test_matrix_reduced[feature + '_rank'] = test_matrix_reduced[feature].rank()
+            # the index of the median value
+            idx = np.nanargmin(np.abs(test_matrix_reduced[feature] - feature_median))
+            median_rank = test_matrix_reduced[feature].iloc[idx]
+            test_matrix_rank_distance[feature] = np.abs(test_matrix_reduced[feature + '_rank'] - median_rank)
+
+        test_matrix_rank_distance = test_matrix_rank_distance.reset_index(drop=True)
+
+        # find the top 5 features where the distance to the median rank value (for standardisation) is the highest
+        # across the top 30 features
+        matrix_transposed = test_matrix_rank_distance.T
+        result = pd.DataFrame(np.zeros((0, 5)), columns=['risk_1', 'risk_2', 'risk_3', 'risk_4', 'risk_5'])
+        for i in matrix_transposed.columns:
+            df1row = pd.DataFrame(matrix_transposed.nlargest(5, i).index.tolist(),
+                                  index=['risk_1', 'risk_2', 'risk_3', 'risk_4', 'risk_5']).T
+            result = pd.concat([result, df1row], axis=0)
+        result = result.reset_index(drop=True)
+
+        # prepare table insert
+        result['entity_id'] = test_matrix_reduced.index.values
+        result['model_id'] = model_id
+        result['as_of_date'] = test_date
+
+        db_conn = self.db_engine.raw_connection()
+
+        # remove all existing evaluations before re-writing
+        query = "DELETE FROM results.individual_importances where model_id = {} and as_of_date = '{}'::TIMESTAMP ".format(
+            model_id, test_date)
+        db_conn.cursor().execute(query)
+        db_conn.commit()
+
+        # write new entries
+        result.to_sql("individual_importances", self.db_engine, if_exists="append", schema="results", index=False)
+
         return None
 
 
