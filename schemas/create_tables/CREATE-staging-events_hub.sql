20cc44bce4eca545d1b19ef987a5d5eb7458026c
klaus <klaus.ackermann@monash.edu>
2017-11-29 16:28:24 -0600

    added a uuid hash field to allow complicated source primary keys

--- schemas/create_tables/CREATE-staging-events_hub.sql
+++ schemas/create_tables/CREATE-staging-events_hub.sql
@@ -4,6 +4,7 @@ CREATE  TABLE staging.events_hub (
   event_chain_id              INT, -- id for the set of events that are linked together
   source_event_id             INT, -- unique id in the source table of this event.
   department_defined_event_id VARCHAR, --
+  department_source_id_hashed UUID, -- note: MD5 hashes can be stored as UUID
   officer_id                  INT REFERENCES staging.officers_hub (officer_id), --
   event_type_code             INT, --
   event_datetime              TIMESTAMP, --
